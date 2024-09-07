5e4306a5ee5449b737aa142a7df3562e87001361
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2024-09-07 09:55:20 +0500

    fix: routes fix

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -6,16 +6,17 @@ import {
     deleteUser
 } from "../controllers/admin.js"
 
+import verifyToken from "../middlewares/verifyToken.js";
 import { isAdmin } from "../middlewares/isAdmin"
 
 import express from "express"
 
 const router = express.Router();
 
-router.get("/users", isAdmin, getAllUsers);
-router.get("/payments", isAdmin, getAllPayments);
-router.get("/tickets", isAdmin, getAllTickets);
-router.get("/events", isAdmin, getAllEvents);
-router.delete("/user/:id", isAdmin, deleteUser);
+router.get("/users", verifyToken, isAdmin, getAllUsers);
+router.get("/payments", verifyToken, isAdmin, getAllPayments);
+router.get("/tickets", verifyToken, isAdmin, getAllTickets);
+router.get("/events", verifyToken, isAdmin, getAllEvents);
+router.delete("/user/:id", verifyToken, isAdmin, deleteUser);
 
 export default router;
