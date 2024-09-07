546bda5b2aea8dedc20cba1bbaf64f0f944251f1
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2024-09-07 10:12:09 +0500

    feat: added events

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -12,9 +12,9 @@ const router = express.Router();
 import verifyToken from '../middlewares/verifyToken.js';
 import { isAgent } from '../middlewares/isAgent.js';
 
-router.get('/agent/events', verifyToken, isAgent, getAgentEvents);
+router.get('/agent', verifyToken, isAgent, getAgentEvents);
 
-router.post('/', createEvent);
+router.post('/', verifyToken, isAgent, createEvent);
 router.get('/all', getEvents);
 router.get('/:id', getEventById);
 
